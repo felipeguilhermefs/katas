626b3fee4d6d84cfc4037412fc90dbd22e0f0bf8
Felipe Flores <felipeguilhermefs@gmail.com>
2024-06-17 11:19:38 +0200

    Use my new lua module

--- balanced-braces/balancedbraces.lua
+++ balanced-braces/balancedbraces.lua
@@ -1,43 +1,43 @@
-local stack = require "stack"
+local stack = require("ff.stack")
 
 local symbols = {
-  ["("] = ")",
-  ["["] = "]",
-  ["{"] = "}",
+	["("] = ")",
+	["["] = "]",
+	["{"] = "}",
 }
 
 symbols.to_closing = function(char)
-  return symbols[char]
+	return symbols[char]
 end
 
 symbols.is_close = function(char)
-  for o,c in pairs(symbols) do
-    if char == c then
-      return true
-    end
-  end
-  return false
+	for o, c in pairs(symbols) do
+		if char == c then
+			return true
+		end
+	end
+	return false
 end
 
 local balancedbraces = {}
 
 function balancedbraces.is_balanced(input)
-  expected = stack()
+	expected = stack()
 
-  for char in input:gmatch"." do
-    local closing = symbols.to_closing(char)
-    if closing then
-      expected.push(closing)
-    end
+	for char in input:gmatch(".") do
+		local closing = symbols.to_closing(char)
+		if closing then
+			expected.push(closing)
+		end
 
-    if symbols.is_close(char) then
-      if expected.pop() ~= char then
-        return false
-      end
-    end
-  end
+		if symbols.is_close(char) then
+			if expected.pop() ~= char then
+				return false
+			end
+		end
+	end
 
-  return expected.empty()
+	return expected.empty()
 end
 
 return balancedbraces
